0ea89497f0f9444aa6e8cb1dd863c3a501b0f623
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2024-11-25 22:06:59 +0600

    add at single
    added at single file

--- single.php
+++ single.php
@@ -1,4 +1,26 @@
-<div>
-    <h1>Custom Page Template</h1>
-    <p>This is a custom page template.</p>
-</div>
+<?php get_header(); ?>
+<main>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+    ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+
+                <div>
+                    <div>
+                        <h1>This is Single page</h1>
+                        <p>Test From Single</p>
+                    </div>
+                    <?php the_content(); ?>
+                </div>
+            </article>
+    <?php
+        endwhile;
+    else :
+        echo '<p>No content found</p>';
+    endif;
+    ?>
+</main>
+
+<?php get_footer(); ?>
